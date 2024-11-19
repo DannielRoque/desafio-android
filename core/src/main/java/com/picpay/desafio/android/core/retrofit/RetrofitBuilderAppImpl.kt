2b4a57c5ac2f3b1168a42deea6c397a6f82744d5
Daniel Roque <P0654608@portoseguro.com.br>
2024-11-19 00:21:19 -0300

    dependencies module updated

--- core/src/main/java/com/picpay/desafio/android/core/retrofit/RetrofitBuilderAppImpl.kt
+++ core/src/main/java/com/picpay/desafio/android/core/retrofit/RetrofitBuilderAppImpl.kt
@@ -4,8 +4,8 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.picpay.desafio.android.core.BaseWebServiceImpl
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import okhttp3.OkHttpClient
+import retrofit2.converter.gson.GsonConverterFactory
 
 internal class RetrofitBuilderAppImpl(
     private val okHttpClient: OkHttpClient
